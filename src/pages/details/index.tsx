293bf272cc1dc6df29604c2f9b7b3673a5896156
stings8 <stingufc@gmail.com>
2020-08-16 23:37:53 -0300

    criado tela de detalhe da vaga

---
+++ src/pages/details/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const Details = () => {
+  return (
+  <View/>);
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: '#f0f0f5'
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
+
+export default Details;
